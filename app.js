aba67fe38a1604a481a20cd48530c8b00dbcc3b8
icedike <icedike@gmail.com>
2020-07-18 20:46:47 +0800

    Add searching function

--- app.js
+++ app.js
@@ -12,13 +12,22 @@ app.set('view engine', 'handlebars')
 // set static directory
 app.use(express.static('public'))
 
+// show restaurant list
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
+// show the restaurant
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
   res.render('show', { restaurant })
+})
+
+// search restaurants by the name
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()))
+  res.render('index', { restaurants, keyword })
 })
 
 app.listen(port, () => {
