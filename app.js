611b33988533964f31fd73c8223d58db41aa8a01
icedike <icedike@gmail.com>
2020-07-19 20:17:55 +0800

    Search by category and show the information for not finding restaurant

--- app.js
+++ app.js
@@ -26,7 +26,9 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
 // search restaurants by the name
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()))
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword)
+  })
   res.render('index', { restaurants, keyword })
 })
 
