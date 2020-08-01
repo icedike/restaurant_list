7d860b99f70a66670a53b21db5ee2a83acf6cc5a
icedike <icedike@gmail.com>
2020-08-01 19:51:37 +0800

    feat: modify seed file

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -10,19 +10,6 @@ db.on('error', () => {
 })
 db.once('open', () => {
   console.log('mongodb connected!')
-  for (let i = 0; i < 8; i++) {
-    Restaurant.create({
-      name: restaurantData[i].name,
-      name_en: restaurantData[i].name_en,
-      category: restaurantData[i].category,
-      image: restaurantData[i].image,
-      location: restaurantData[i].location,
-      phone: restaurantData[i].phone,
-      google_map: restaurantData[i].google_map,
-      rating: restaurantData[i].rating,
-      description: restaurantData[i].description
-    })
-  }
-
+  restaurantData.forEach(data => Restaurant.create(data))
   console.log('Done')
 })
