81c61277ab6b0854e5192201d960daab3567a070
icedike <icedike@gmail.com>
2020-08-23 22:52:19 +0800

    feat: modify seed

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use((req, res, next) => {
   res.locals.user = req.user
   res.locals.success_msg = req.flash('success_msg')
   res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.login_msg = req.flash('error')
   next()
 })
 
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,11 +1,48 @@
 const { results } = require('./restaurant.json')
-const Restaurant = require('../restaurant') // 載入 restaurant model
-
+const Restaurant = require('../restaurant')
+const User = require('../user')
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const db = require('../../config/mongoose')
 
 const restaurantData = results
+const SEED_USER = [{
+  name: 'User1',
+  email: 'user1@example.com',
+  password: '12345678'
+},
+{
+  name: 'User2',
+  email: 'user2@example.com',
+  password: '12345678'
+}
+]
 
-db.once('open', () => {
-  restaurantData.forEach(data => Restaurant.create(data))
-  console.log('Done')
+db.once('open', async () => {
+  for (let i = 0; i < 2; i++) {
+    await bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(SEED_USER[i].password, salt))
+      .then(hash => User.create({
+        name: SEED_USER[i].name,
+        email: SEED_USER[i].email,
+        password: hash
+      }))
+      .then(user => {
+        const userId = user._id
+        return Promise.all(Array.from(
+          { length: 3 },
+          (_, j) => {
+            return Restaurant.create(Object.assign(restaurantData[j + i * 3], { userId }))
+          }
+        ))
+      })
+      .then(() => {
+        console.log(`User${i + 1} done!`)
+      })
+      .catch(error => console.log(error))
+  }
+  process.exit()
 })
