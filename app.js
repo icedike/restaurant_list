36f8308712100a94098391e3905a0d816435062d
icedike <icedike@gmail.com>
2020-08-23 21:17:38 +0800

    feat: add facebbok login & dotenv

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const flash = require('connect-flash')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const usePassport = require('./config/passport')
 const routes = require('./routes')
 require('./config/mongoose')
 
-const port = 3000
+const port = process.env.PORT
 const app = express()
 
 // set template engine
