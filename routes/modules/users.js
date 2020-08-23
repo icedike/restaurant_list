a18a3f1e6aaa882b4b75491f3d64745931643970
icedike <icedike@gmail.com>
2020-08-23 20:49:58 +0800

    feat: add bcryptjs

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 
 const passport = require('passport')
 const User = require('../../models/user')
@@ -38,22 +39,25 @@ router.post('/register', (req, res) => {
   User.findOne({ email }).then(user => {
     // 如果已經註冊：退回原本畫面
     if (user) {
-      res.render('register', {
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      return res.render('register', {
+        errors,
         name,
         email,
         password,
         confirmPassword
       })
-    } else {
-      // 如果還沒註冊：寫入資料庫
-      return User.create({
+    }
+    return bcrypt
+      .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+      .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+      .then(hash => User.create({
         name,
         email,
-        password
-      })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
-    }
+        password: hash // 用雜湊值取代原本的使用者密碼
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
   })
 })
 
